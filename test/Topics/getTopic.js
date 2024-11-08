5ef7a019d4393b19a8693e18ae66a33af5716a89
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-11-08 18:56:02 +0500

    Get Topic added

---
+++ test/Topics/getTopic.js
@@ -0,0 +1,25 @@
+// ? API: /api/topic/${fannomi}/${mavzuNomi}/
+
+// ? QAYTADIGAN MALUMOT
+// {
+//   name: '',
+//   desc: '',
+//   embed: '',
+//   _id: ''
+// }
+
+// ? Template
+const fannomi = "Matematika";
+const mavzuNomi = "1-dars: Algebra";
+
+fetch(`http://localhost:5000/api/topic/${fannomi}/${mavzuNomi}/`)
+  .then((response) => {
+    if (!response.ok) {
+      return response.text().then((text) => {
+        throw new Error(`Xatolik: ${text}`);
+      });
+    }
+    return response.json();
+  })
+  .then((data) => console.log("Barcha mavzular:", data))
+  .catch((error) => console.error("Xatolik:", error));
